d0b67bd44b7ea233181f0cfa27811387f26b0d7d
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2024-12-27 03:37:46 +0530

    todo app done with react and vanilla ts -> hands on Ts practice

---
+++ todo/src/main.ts
@@ -0,0 +1,77 @@
+import './style.css'
+
+interface Todo{
+  title: string,
+  isCompleted: boolean,
+  readonly id: string
+}
+
+const todos: Todo[] = []
+
+const todosContainer = document.querySelector(".todoContainer") as HTMLDivElement
+
+const todoInput = document.getElementsByName("title")[0] as HTMLInputElement
+
+const myForm = document.getElementById("myform") as HTMLFormElement
+
+
+myForm.onsubmit = (e:SubmitEvent) => {
+  e.preventDefault()
+
+  const todo: Todo = {
+    title: todoInput.value,
+    isCompleted: false,
+    id: String(Math.random()*1000)
+  }
+
+  todos.push(todo)
+  todoInput.value = ""
+
+  renderTodo(todos)
+}
+
+
+const generateTodoItem = (title: string, isCompleted: boolean, id:string) => {
+  const todo:HTMLDivElement = document.createElement('div')
+  todo.className = "todo"
+  const checkBox:HTMLInputElement = document.createElement("input")
+  checkBox.setAttribute("type", "checkbox")
+  checkBox.className = "isCompleted"
+  checkBox.checked = isCompleted
+
+  checkBox.onchange = () => {
+    todos.find(item => {
+      if(item.id === id) item.isCompleted = checkBox.checked
+    })
+    paragraph.className = checkBox.checked ? "textCut" : ""
+  }
+
+  const paragraph:HTMLParagraphElement = document.createElement("p")
+  paragraph.innerText = title
+  paragraph.className = checkBox.checked ? "textCut" : ""
+
+  const btn:HTMLButtonElement = document.createElement('button')
+  btn.innerText = "X"
+  btn.className = "deleteBtn"
+  btn.onclick = () => {
+    deleteTodo(id)
+  }
+
+
+  todo.append(checkBox, paragraph, btn)
+  todosContainer.append(todo)
+}
+
+const deleteTodo = (id:string) => {
+  const idx = todos.findIndex((Item) => Item.id === id)
+
+  todos.splice(idx, 1)
+  renderTodo(todos)
+}
+
+const renderTodo = (todos: Todo[]) => {
+  todosContainer.innerText = ""
+  todos.forEach((item) => {
+    generateTodoItem(item.title, item.isCompleted, item.id)
+  })
+}
