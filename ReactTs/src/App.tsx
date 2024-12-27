fff69fe978b02a2d6bc480ce51333b32d9cf9e2e
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2024-12-27 22:03:09 +0530

    Ts done along with basic react ts

--- ReactTs/src/App.tsx
+++ ReactTs/src/App.tsx
@@ -1,15 +1,132 @@
-import Box from "./components/Box"
+import { useState, FormEvent, createContext } from "react"
+// import Box from "./components/Box"
+
+
+
+// props demonstration ;- 
+
+// function App(){
+//   return (
+//     <>
+
+//       <div>
+//         <Box children = {<h1>Wow Nice</h1>}/>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+//note since we have defined the type of children props as ReactNode in Box component, we can use Box component as wrapper and send any component, string, etc wrapped around the box component and it will still be treated as children
+
+// wrapper demonstration
+
+// function App(){
+//   return (
+//     <>
+
+//       <div>
+//         <Box heading={"HELLO WORLD"} count={34}>
+//           {/* anything written below inside the braces will be treated as choildren, we can send it as props too in a self closing tag also */}
+//           {<button>Click</button>}
+//         </Box>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+//Demonstrating Generic
+
+// function App(){
+
+//   const [val, setVal] = useState<string>("")
+
+//   return (
+//     <>
+
+//       <div>
+//         <Box label="Search" value = {val} setter={setVal}/>
+//       </div>
+
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+//form handling
+// interface Person{
+//   name: string
+//   age: number
+// }
+
+// function App(){
+
+//   const [user, setUser] = useState<Person>() //user can be person or undefined, to handle undefined add a dummy obj as initial value or use ! in prev operator as done below
+
+//   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault()
+//     console.log(user)
+//   }
+
+//   return (
+//     <>
+//       <div>
+//         <form onSubmit={submitHandler}>
+
+//           <input type="number"
+//            value={user?.age || ""} //for maintaining type safety as initially the value changes from undefined to efined so we give an empty string by default
+//            placeholder="Age"
+//            onChange={ (e) => setUser((prev) => ({...prev!, age:Number(e.target.value) }))} //using the spread operator to use setUser as its an objec so we need to spread the entire existing obj (empty -> in case of first input and prev values in case we enter again) and then make changes using setUser in the age field
+//           />
+
+//           <input type="text"
+//            value={user?.name || ""} //for maintaining type safety as initially the value changes from undefined to efined so we give an empty string by default
+//            placeholder="Name"
+//            onChange={ (e) => setUser((prev) => ({...prev!, name:(e.target.value) }))} //using the spread operator to use setUser as its an objec so we need to spread the entire existing obj (empty -> in case of first input and prev values in case we enter again) and then make changes using setUser in the name field
+//           />
+
+//           <button type="submit">Submit</button>
+//         </form>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+
+
+//use context in react with ts
+
+type ThemeType = "light" | "dark"
+
+interface ThemeContextType{
+  theme: ThemeType,
+  toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: "light", //default val
+  toggleTheme: () => {}
+})
+
+
 
 function App(){
-  return (
-    <>
-
-      <div>
-        <Box heading={"HELLO WORLD"} count={34} func1 = {(a:string) => alert(a)}/>
-      </div>
-
-    </>
-  )
+  return <div>Hello</div>
 }
 
 export default App
--- ReactTs/src/components/Box.tsx
+++ ReactTs/src/components/Box.tsx
@@ -1,5 +1,6 @@
-//M1
+import { Dispatch, SetStateAction } from "react"
 
+//M1
 // const Box = ({heading, subheading}:{heading:string, subheading:string}) => {
 //   return (
 //     <div>
@@ -11,6 +12,9 @@
 // }
 
 // export default Box
+
+
+//M2 type defining props
 
 // type propsType = {
 //     heading: string
@@ -27,21 +31,90 @@
 // export default Box
 
 
-//ME -> DESTRUCTURED PROPS
-type propsType = {
-    heading: string
-    count: number
-    func1: (a:string) => void
-}
+//DESTRUCTURED PROPS
 
-const Box = ({heading, count, func1}:propsType) => {
-    func1("adbahd")
+// type propsType = {
+//     heading: string
+//     count: number
+//     func1: (a:string) => void
+// }
+
+// const Box = ({heading, count, func1}:propsType) => {
+//     func1("adbahd")
+//     return (
+//         <div>
+//             <h1>{heading}</h1>
+//             <p>{count}</p>
+//         </div>
+//     )
+// }
+
+// export default Box
+
+
+
+//Demonstrating children as props
+
+// type propsType = {
+//     heading: string
+//     count: number
+//     children: ReactNode
+// }
+
+// const Box = ({heading, count, children}:propsType) => {
+//     return (
+//         <div>
+//             <h1>{heading}</h1>
+//             <p>{count}</p>
+//             {children}
+//         </div>
+//     )
+// }
+
+// export default Box
+
+
+//default value assigning to props
+//NOTE - > FOR DEFAULT ASSIGNING THE FIELDS MUST BE MARKED OPTIONAL AS IT INFORMS TS TO NOT THROW ERROR WE DEFAULT PROPS ARE NOT PASSED TO THE COMPONENT AND USE THE DEFAULT ONES SET IN THE COMPONENT ITSELF, MARKING OPTIONAL ALSO MAKES IT ASSERTIVE THAT IT IS ABSOLUTELY OK TO NOT SEND ANY DATA FOR THAT PROPS NEITHER EXPLICIT PASSING NOR DEFAULT PASSING
+
+
+//for the below syntax count is marked optional has a default value :- 
+// 1. if no value is passed to it explicitly, default value set here is passed.
+// 2. if a value is passed explicitly and it also has a default value set, the explicit value will be considered.
+//3. if no value altogether is passed, neither explicit nor default value, then it wont throw any error as we have marked it optional.
+
+// type propsType = {
+//     heading?: string
+//     count?: number
+//     children: ReactNode
+// }
+
+// const Box = ( {heading = "New Head", count, children} : propsType ) => {
+//     return (
+//         <div>
+//             <h1>{heading}</h1>
+//             { count && <p>{count}</p>}
+//             {children}
+//         </div>
+//     )
+// }
+
+// export default Box
+
+
+
+//VVVIIII DEMONSTRATING GENERIC
+
+type InputValType = string | number
+
+const Box = <T extends InputValType>( {label, value, setter} : {label:string, value:T, setter: Dispatch<SetStateAction<T>>} ) => {
     return (
-        <div>
-            <h1>{heading}</h1>
-            <p>{count}</p>
-        </div>
+        <form>
+            <label>{label}</label>
+            <input type="text" value={value} onChange={(e) => setter(e.target.value as T)}/>
+        </form>
     )
 }
 
 export default Box
+
