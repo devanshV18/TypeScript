d688c4b664e0d384fa73dd5de79412c3e19b6e15
Devansh Verma <143462246+devanshV18@users.noreply.github.com>
2025-01-02 03:43:46 +0530

    done with all components - Typescript good understanding and learning done

--- TsAPiProject/src/components/Result.tsx
+++ TsAPiProject/src/components/Result.tsx
@@ -1,9 +1,80 @@
+import { Button, Container, List, ListItem, Stack, Typography } from "@mui/material"
+import { useSelector, useDispatch} from "react-redux"
+import { clearState } from "../Redux/slices"
+import { useNavigate } from "react-router-dom"
+import { countMatchingElements } from "../utils/features"
+
+const result = ["Lol", "Sample", "Ans"]
 
 const Result = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const {words, result} = useSelector((state:{
+    root: StateType
+  }) => state.root)
+
+  const correctAns = countMatchingElements(result, words.map((i) => i.meaning))
+  const percentage = (correctAns / words.length) * 100
+
+  const resetHandler = () => {
+    navigate("/")
+    dispatch(clearState())
+  }
+
   return (
-    <div>
-      Result
-    </div>
+    <Container maxWidth={"sm"}>
+      <Typography variant="h3" color={"primary"} m={"2rem 0"}>
+        Result
+      </Typography>
+
+      <Typography m = {"1rem"} variant="h6">
+        You got {correctAns} right out of {words?.length}
+      </Typography>
+      
+      <Stack direction={"row"} justifyContent={"space-evenly"}>
+        <Stack>
+          <Typography m={"1rem 0"} variant="h5">
+            Your Ans
+          </Typography>
+          <List>
+            {result.map((i,idx) => (
+              <ListItem key={idx}>
+                {idx+1} - {i}
+              </ListItem>
+            ))}
+          </List>
+        </Stack>
+        
+        <Stack>
+          <Typography m={"1rem 0"} variant="h5">
+            Correct Ans
+          </Typography>
+
+          <List>
+            {words?.map((i,idx) => (
+              <ListItem key={idx}>
+                {idx+1} - {i.meaning}
+              </ListItem>
+            ))}
+          </List>
+        </Stack>
+      </Stack>
+
+      <Typography
+        m={"1rem"}
+        variant="h5"
+        color={percentage > 50 ? "green" : "red"}
+      >
+        {percentage}% -
+        {percentage > 50 ? "Pass" : "Fail"}
+      </Typography>
+
+      <Button sx = {{margin: "1rem"}} variant="contained" onClick={resetHandler}>
+        Reset
+      </Button>
+
+    </Container>
   )
 }
 
--- TsAPiProject/src/utils/features.ts
+++ TsAPiProject/src/utils/features.ts
@@ -58,3 +58,17 @@ export const translateWords = async(params: LangTypeCode) => {
         throw new Error("Something Went wrong")
     }
 }
+
+export const countMatchingElements = (arr1: string[], arr2:string[]): number => {
+    if(arr1.length !== arr2.length){
+        throw new Error("Unequal Arrays")
+    }
+
+    let matchingCount = 0
+
+    for(let i = 0; i < arr1.length; i++){
+        if(arr1[i] === arr2[i]) matchingCount++
+    }
+
+    return matchingCount
+}
